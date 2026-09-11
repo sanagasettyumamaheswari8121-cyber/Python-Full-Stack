fed69ca7c93d2bf30c992efc7e088db30c883265
Sanagasetty Uma Maheswari <sanagasettyumamaheswari8121@gmail.com>
2026-09-11 15:59:08 +0530

    Add files via upload

---
+++ if condition by using 4 operators.py
@@ -0,0 +1,141 @@
+#if condition by using comparision operators
+#<,>,<=,>=,!=,==
+'''a=10
+b=20
+if a<b:
+    print("true")'''
+
+'''a=40
+b=60
+if a>b:
+    print("less")'''
+
+'''a=50
+b=80
+if b>a:
+    print("greater")'''
+
+'''a=5
+b=9
+if a<=b:
+    print("lesser")'''
+
+'''a=11
+b=13
+if b>=a:
+    print("greater")'''
+
+'''a=7
+b=8
+if a!=b:
+    print("not equal")'''
+
+'''a=5
+b=5
+if a==b:
+    print("equal")'''
+
+'''a="python"
+if a=="python":
+    print("true")'''
+
+'''a="java"
+if a!="python":
+    print("false")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+'''a=int(input("a value"))
+if a>30:
+    print("true")'''
+
+#if condition by using logical operators
+# and, or, not
+'''a=4
+b=8
+if a<b and b>a:
+    print("less")'''
+
+'''a=6
+b=9
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=7
+b=10
+if a!=b and b==a:
+    print("less")'''
+
+'''a=12
+b=14
+if a<b or b>a:
+    print("less")'''
+
+'''a=15
+b=20
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=10
+b=20
+if a!=b or b==a:
+    print("less")'''
+
+'''a=3
+b=5
+if not a<b:
+    print("true")'''
+
+'''a=3
+b=5
+if not a<b and b>a:
+    print("true")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<=b and b>=a:
+    print("true")'''
+
+#if condition using identify operators
+#is, is not
+'''a=10
+if type(a) is int:
+    print("it is int")'''
+
+'''a=7
+if type(a) is not int:
+    print("false")'''
+
+'''a=10.5
+if type(a) is not int:
+    print("it is not int")'''
+
+'''a=int(input("a value"))
+if type(a) is int:
+    print("it is int")'''
+
+#if condition by using membership operators
+#in, not in
+'''a=[2,3,4,5,6,7,8,9,10]
+if 5 in a:
+    print("true")'''
+
+'''a=[2,3,4,5,6,7,8,9,10]
+if 10 not in a:
+    print("true")'''
+
+'''a=[2,3,4,5,6,7,8,9,10]
+if 51 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")'''
+
+'''a=[2,3,4,5,6,7,8,9,10]
+b=int(input("enter the number"))
+if b in a:
+    print("true")'''
---
+++ if else by using 4 operators.py
@@ -0,0 +1,61 @@
+#if else condition by using comparision operators
+'''a=5
+b=9
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+'''a=5
+b=9
+if a>b:
+    print("less")
+else:
+    print("false")'''
+#if else condition by using logical operators
+'''a=4
+b=5
+if a<b and a==b:
+    print("true")
+else:
+    print("false")'''
+
+'''a=6
+b=9
+if a<b or a>b:
+    print("false")
+else:
+    print("true")'''
+
+'''a=3
+b=5
+if not a<b not b>a:
+    print("true")
+else:
+    print("false")'''
+
+#if else condition by using identify operators
+'''a=7
+if type(a) is int:
+    print("true")
+else:
+    print("false")'''
+
+'''a=10.5
+if type(a) is not int:
+    print("it is float")
+else:
+    print("it is int")'''
+
+#if else condition by using membership operators
+'''a=[5,6,7,8,9,10]
+if 5 in a:
+    print("is there")
+else:
+    print("not there")'''
+
+'''a=[2,3,4,5,6,7,8,9,10]
+if 55 not in a:
+    print("not there")
+else:
+    print("is there")'''
