d532b0dd1e735f5dcf2fb815f23ab966933b0912
Sanagasetty Uma Maheswari <sanagasettyumamaheswari8121@gmail.com>
2026-09-16 22:18:45 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,112 @@
+#loops
+#for, while, range, break, continue, pass
+
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50,60]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,"end")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=(6,7,8,9,10)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={1,2,3,4,5,6}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={"year":2026,"month":"sep","date":16}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={"year":2026,"month":"sep","date":16}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))
+print(type(a))
+print(type(i))'''
+
+'''a=[4.5,5.2,6.2,7.3]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=["python","java","c"]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[4+7j,5+9j,8+6j]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[True, False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[4,4.5,"python",4+6j,True,False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#task by using for loop
+a=["apple","banana","grapes"]
+# output: ["APPLE","BANANA", "GRAPES"]
+'''a=["apple","banana","grapes"]
+for i in a:
+    print(i.upper())'''
+
+#output as a list
+'''a=["apple","banana","grapes"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)''' //['APPLE', 'BANANA', 'GRAPES']
+
+
---
+++ tasks on conditions.py
@@ -0,0 +1,83 @@
+#voting by using if else
+'''age=int(input("enter the age"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+#even or odd by using if else
+'''number=int(input("enter the number"))
+if number%2==0:
+    print("It is an even no.")
+else:
+    print("It is an odd no.")'''
+
+#leap year by using if else
+'''year=int(input("enter the year"))
+if year%4==0:
+    print("it is a leap year")
+else:
+    print("it is not a leap year")'''
+
+#vowels by using if else
+'''vowels=["a","e","i","o","u"]
+a=input("enter the letter")
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+
+'''vowels=["a","e","i","o","u"]
+a=input("enter the letter").lower()
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+
+#guest code by using if else
+'''name=input("enter the name")
+if name=="uma":
+    print("Welcome",name)
+else:
+    print("Welcome Guest")'''
+
+'''name=input("enter the name").lower()
+if name=="uma":
+    print("Welcome",name)
+else:
+    print("Welcome Guest")'''
+
+'''names=["uma","sowmya","pushpa","arshitha"]
+a=input("enter the name").lower()
+if a in names:
+    print("Welcome", a)
+else:
+    print("Welcome guest")'''
+
+#bakery code by using if elif else
+'''cake_price=int(input("enter the cake price"))
+if cake_price==1200:
+    print("Redvelvet Cake")
+elif cake_price==1000:
+    print("Choco Almond")
+elif cake_price==800:
+    print("Chocolate Cake")
+elif  cake_price==600:
+    print("Butterscrotch")
+else:
+    print("Sorry your desired cake is not available right now")'''
+
+#pizza code by using if elif else
+'''a=input("Enter the pizza name")
+if a=="crispy chicken pizza":
+    print(800)
+elif a=="bbq pizza":
+    print(600)
+elif a=="panner pizza":
+    print(400)
+elif a=="corn":
+    print(300)
+elif a=="french fries & coke":
+    print(200)
+else:
+    print("Sorry your desired item is not available")'''
