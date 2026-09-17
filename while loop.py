9398e26ee9eeb9d61184c00f708253aa9eda7c47
Sanagasetty Uma Maheswari <sanagasettyumamaheswari8121@gmail.com>
2026-09-17 21:40:48 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,52 @@
+#while loop
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a<1:
+    print(a)'''
+
+'''a=20
+while a>=1:
+    print(a)
+    a=a-1'''
+
+'''a=20
+while a>1:
+    a=a-1
+    print(a)'''
+
+'''a=20
+while a>1:
+    a=a-1
+print(a)'''
+
+'''a=15
+while a>2:
+    print(a)
+    a=a-1'''
+#don't
+'''a=30
+while a>1:
+    print(a)
+    a+=1'''
+#do       big no. - use decrement 
+'''a=30
+while a>1:
+    print(a)
+    a-=1'''
+
+#do       small no. - use increment
+'''a=5
+while a<15:
+    print(a)
+    a+=1'''
+
+#voting by using while loop (use while True for run tym input)
+'''while True:
+        age=int(input("Enter the age: "))
+        if age>=18:
+            print("Eligible for vote")
+        else:
+            print("Not eligible for vote")'''
