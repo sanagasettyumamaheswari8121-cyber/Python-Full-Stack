bcc93df3b9bffd62dcac95a562bfa56655147b36
Sanagasetty Uma Maheswari <sanagasettyumamaheswari8121@gmail.com>
2026-09-26 20:47:35 +0530

    Add files via upload

---
+++ tasks on nested & multiple if conditions.py
@@ -0,0 +1,35 @@
+#social-media login by using nested if   // if inside another if
+'''username=input("enter the username")
+password=int(input("enter the password"))
+if username=="uma":
+    if password==1234:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("invalid username")'''
+
+'''username=input("enter the username")
+password=int(input("enter the password"))
+if username=="uma" and password==1234:
+    print("login successful")
+else:
+    print("invalid username")'''
+
+'''username=input("enter the username")
+password=input("enter the password")
+if username=="uma" and password=="uma@1234":
+    print("login successful")
+else:
+    print("invalid username")'''
+
+#student data by using multiple if     // 3 conditions should be true & satisfied
+'''age=int(input("age"))
+attendence=int(input("attendence"))
+marks=int(input("marks"))
+if age>=18:
+    print("They are eligible for vote")
+if attendence>=70:
+    print("They are allow to write exams")
+if marks>=80:
+    print("They are ready for scholarship")'''
